b4610c3c1b3d5f1b9b1ce1078c9aeff8ea8f7274
Pedro Aguiar <paguiar32@gmail.com>
2013-10-04 08:18:05 -0500

    Hardware controller class foundation

--- RTCs/hardware/src/Hardware.cpp
+++ RTCs/hardware/src/Hardware.cpp
@@ -6,9 +6,26 @@
  *
  * $Id$
  */
+#include <sstream>
 #include <iostream>
 #include <json/json.h>
 #include "Hardware.h"
+
+#define EXEC_DELAY 1000
+
+enum {
+        HW_CURRENT=0,
+        HW_TARGET,
+        HW_COUNT
+};
+
+enum {
+        ANGLE_1=0,
+        ANGLE_2,
+        ANGLE_3,
+	ANGLE_GRIPPER,
+	ANGLE_COUNT
+};
 
 // Module specification
 // <rtc-template block="module_spec">
@@ -28,6 +45,75 @@ static const char* hardware_spec[] =
     ""
   };
 // </rtc-template>
+
+class H9_Hardware {
+ private:
+	float slopes[ANGLE_COUNT];
+	float angles[HW_COUNT][ANGLE_COUNT];
+	int updated;
+ public:
+	H9_Hardware();
+	int setValues(float*, float*, unsigned short int);
+	std::string getJSON(unsigned short int t);
+	void setSlopes(float*);
+	void doUpdate(void);
+} HW;
+
+void H9_Hardware::setSlopes(float *s){
+	setValues(NULL, s, 0);
+}
+
+void H9_Hardware::doUpdate(void){
+	for(int i=0;i<ANGLE_COUNT;i++){
+		if (angles[HW_CURRENT][i] != angles[HW_TARGET][i])
+		{
+			updated = 1;
+			float smaller_current = (((float)(angles[HW_CURRENT][i]<angles[HW_TARGET][i]))-0.5f)*2.0f;	//Ugly way to get -1 or 1
+			float diff = (angles[HW_TARGET][i]-angles[HW_CURRENT][i])*smaller_current;
+			angles[HW_CURRENT][i] += (diff>slopes[i] ? slopes[i]: diff)*smaller_current;			//Should I use setValues()? :P
+		}
+	}
+}
+
+H9_Hardware::H9_Hardware(){
+// Read these from the robot
+	float slopes[4] = {0.5f, 0.5f, 0.5f, 0.5f};
+	this->setSlopes(slopes);
+	float initial_angles[4] = {0.0f, 45.0f, -10.0f, 0.0f};
+	this->setValues(initial_angles, NULL, HW_CURRENT);
+// These will be received from the master block
+	float t_angles[4] = {1.1f, 0.0f, 10.0f, 1.0f};
+	this->setValues(t_angles, NULL, HW_TARGET);
+}
+
+int H9_Hardware::setValues(float* a, float* s, unsigned short int t = 0){
+	if(a){
+		for(int i=0;i<ANGLE_COUNT;i++){
+			angles[t][i]=a[i];
+		}
+	}
+
+	if(s){
+		for(int i=0;i<ANGLE_COUNT;i++){
+			slopes[i]=s[i];
+		}
+	}
+	return 0;
+}
+
+std::string H9_Hardware::getJSON(unsigned short int t = 0){
+
+	std::ostringstream out;
+	out << "<WRIST>([ANGLE.1:" << angles[t][ANGLE_1] << "][ANGLE.2:" << angles[t][ANGLE_2] << "][ANGLE.3:" << angles[t][ANGLE_3] << "][GRIPPER:" << angles[t][ANGLE_GRIPPER] << "])";
+	Json::Value toRobot;
+	toRobot["ns"] = "org.jwebsocket.plugins.system";
+	toRobot["type"] = "broadcast";
+	toRobot["data"] = out.str();
+	toRobot["responseRequested"] = true;
+	toRobot["utid"] = 1;
+
+ return toRobot.toStyledString();
+}
 
 /*!
  * @brief constructor
@@ -89,18 +175,13 @@ RTC::ReturnCode_t Hardware::onFinalize()
 
 RTC::ReturnCode_t Hardware::onStartup(RTC::UniqueId ec_id)
 {
-	Json::Value toRobot;
 
-	toRobot["ns"] = "org.jwebsocket.plugins.system";
-	toRobot["type"] = "broadcast";
-	toRobot["data"] = "<WRIST>([ANGLE.1:DEGREES][ANGLE.2:DEGREES][ANGLE.3:DEGREES][GRIPPER:OPENCLOSED])";
-	toRobot["responseRequested"] = true;
-	toRobot["utid"] = 1;
-
-	// Output to see the result
-	std::cout<<"creating nested Json::Value Example pretty print: "
-	<<std::endl<<toRobot.toStyledString()
-	<<std::endl;
+	for(int xx=0;xx<10;xx++)
+	{
+		std::cout << HW.getJSON(HW_CURRENT) << std::endl;
+		HW.doUpdate();
+		coil::usleep(EXEC_DELAY);
+	}
 
   return RTC::RTC_OK;
 }
@@ -136,20 +217,11 @@ RTC::ReturnCode_t Hardware::onExecute(RTC::UniqueId ec_id)
 
 // Creation//
 
-/*
-
-Tipo: String (concatenado)
-"{\"ns\":\"org.jwebsocket.plugins.system\",\"type\":\"broadcast\",\"data\":\"MESSAGE\",\"responseRequested\":true,\"utid\":VALUE}"
-
-MESSAGE = "<WRIST>([ANGLE.1:DEGREES][ANGLE.2:DEGREES][ANGLE.3:DEGREES][GRIPPER:OPENCLOSED])"
-VALUE = Número de serie del mensaje (incremental).
-DEGREES = El valor del ángulo en grados (rango de -45 a 45).
-OPENCLOSED = 0 es cerrado, 1 es abierto
-*/
 
 
 
-  coil::usleep(1000);
+
+  coil::usleep(1000000);
   return RTC::RTC_OK;
 }
 
