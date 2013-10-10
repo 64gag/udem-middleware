7480fba2497e633b6734d54081a4bbdf7e8fa2da
Pedro Aguiar <paguiar32@gmail.com>
2013-10-10 00:55:17 -0500

    Code adapted to run on RTC, pending: ports and configuration

--- RTCs/vision/src/Vision.cpp
+++ RTCs/vision/src/Vision.cpp
@@ -36,6 +36,15 @@ using namespace cv;
 std::string path = "/home/paguiar/Dropbox/UDEM/PPD - Middleware/Vision/src/";
 std::string file[TARGET_COUNT] = { "fast.jpg", "slow.jpg", "left.jpg", "right.jpg", "stairs.jpg", "elevator.jpg", "stop.jpg" };
 
+
+/* Globals are ugly... but fast for prototyping. Change later */
+Mat *t_images = NULL;
+Mat *t_descriptors = NULL;
+std::vector<KeyPoint> * t_kp = NULL;
+std::vector<Point2f> * t_ob_corners = NULL;
+SurfFeatureDetector * detector = NULL;
+SurfDescriptorExtractor * extractor = NULL;
+FlannBasedMatcher * matcher = NULL;
 unsigned int gFlags = 0;
 
 // Module specification
@@ -131,80 +140,82 @@ RTC::ReturnCode_t Vision::onShutdown(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t Vision::onActivated(RTC::UniqueId ec_id)
 {
+	gFlags |= FLAG_DRAW;
+
+	t_images = new Mat[TARGET_COUNT];
+	t_descriptors = new Mat[TARGET_COUNT];
+	t_kp = new std::vector<KeyPoint>[TARGET_COUNT];
+	t_ob_corners = new std::vector<Point2f>[TARGET_COUNT];
+	detector = new SurfFeatureDetector(300);
+	extractor = new SurfDescriptorExtractor();
+	matcher = new FlannBasedMatcher();
+
+	for (int i=0; i<TARGET_COUNT; i++){
+		t_images[i] = imread( path+file[i], CV_LOAD_IMAGE_GRAYSCALE );
+		detector->detect( t_images[i], t_kp[i]);
+		extractor->compute( t_images[i], t_kp[i], t_descriptors[i] );
+		t_ob_corners[i].push_back(cvPoint(0,0));
+		t_ob_corners[i].push_back(cvPoint( t_images[i].cols, 0 ));
+		t_ob_corners[i].push_back(cvPoint( t_images[i].cols, t_images[i].rows ));
+		t_ob_corners[i].push_back(cvPoint( 0, t_images[i].rows ));
+	}
+
   return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t Vision::onDeactivated(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
+	delete[] t_images;
+	delete[] t_descriptors;
+	delete[] t_kp;
+	delete[] t_ob_corners;
+	delete detector;
+	delete extractor;
+	delete matcher;
+
+ return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t Vision::onExecute(RTC::UniqueId ec_id)
 {
-gFlags |= FLAG_DRAW;
+	double *large = new double[TARGET_COUNT];
+	double *width = new double[TARGET_COUNT];
+	double *area = new double[TARGET_COUNT];
 
-Mat *t_images = new Mat[TARGET_COUNT];
-Mat *t_descriptors = new Mat[TARGET_COUNT];
-std::vector<KeyPoint> * t_kp = new std::vector<KeyPoint>[TARGET_COUNT];
-std::vector<Point2f> * t_ob_corners = new std::vector<Point2f>[TARGET_COUNT];
+	gFlags |= FLAG_DRAW;
 
-int minHessian = 300;
-SurfFeatureDetector detector( minHessian );
-SurfDescriptorExtractor extractor;
-FlannBasedMatcher matcher;
-
-for (int i=0; i<TARGET_COUNT; i++){
-	t_images[i] = imread( path+file[i], CV_LOAD_IMAGE_GRAYSCALE );
-	detector.detect( t_images[i], t_kp[i]);
-	extractor.compute( t_images[i], t_kp[i], t_descriptors[i] );
-	t_ob_corners[i].push_back(cvPoint(0,0));
-	t_ob_corners[i].push_back(cvPoint( t_images[i].cols, 0 ));
-	t_ob_corners[i].push_back(cvPoint( t_images[i].cols, t_images[i].rows ));
-	t_ob_corners[i].push_back(cvPoint( 0, t_images[i].rows ));
-}
-
-    VideoCapture cap(0);
-
-    char key = 'a';
-    int framecount = 0;
-    while (key != 'q')
-    {
-        Mat frame;
-        cap >> frame;
-
-        if (framecount < 9)
-        {
-            framecount++;
-            continue;
-        }
+	Mat frame;
+	VideoCapture cap(0);
+	cap >> frame;
 
 	std::vector<vector<DMatch > > *t_matches = new std::vector<vector<DMatch > >[TARGET_COUNT];
 	std::vector<DMatch > *t_good_matches = new std::vector<DMatch >[TARGET_COUNT];
 	std::vector<Point2f> *t_ob = new std::vector<Point2f>[TARGET_COUNT];
+	std::vector<Point2f> *t_ob_center = new std::vector<Point2f>[TARGET_COUNT];
 	std::vector<Point2f> *t_scene = new std::vector<Point2f>[TARGET_COUNT];
 	std::vector<Point2f> *t_scene_corners = new std::vector<Point2f>[TARGET_COUNT];
 
-        Mat des_image, img_matches;
-        std::vector<KeyPoint> kp_image;
+	Mat des_image, img_matches;
+	std::vector<KeyPoint> kp_image;
 
-        Mat H;
-        Mat image;
+	Mat H;
+	Mat image;
 
-        cvtColor(frame, image, CV_RGB2GRAY);
+	cvtColor(frame, image, CV_RGB2GRAY);
 
-        detector.detect( image, kp_image );
-        extractor.compute( image, kp_image, des_image );
+	detector->detect( image, kp_image );
+	extractor->compute( image, kp_image, des_image );
 
 	for(int t=0; t<TARGET_COUNT; t++){
 		t_scene_corners[t].resize(4);
-		matcher.knnMatch(t_descriptors[t], des_image, t_matches[t], 2);
+		matcher->knnMatch(t_descriptors[t], des_image, t_matches[t], 2);
 		for(int i = 0; i < min(des_image.rows-1,(int) t_matches[t].size()); i++) //THIS LOOP IS SENSITIVE TO SEGFAULTS
 		{
 		    if((t_matches[t][i][0].distance < 0.6*(t_matches[t][i][1].distance)) && ((int) t_matches[t][i].size()<=2 && (int) t_matches[t][i].size()>0))
 		    {
-		        t_good_matches[t].push_back(t_matches[t][i][0]);
+			t_good_matches[t].push_back(t_matches[t][i][0]);
 		    }
 		}
 		//Draw only "good" matches
@@ -212,12 +223,9 @@ for (int i=0; i<TARGET_COUNT; i++){
 
 		if (t_good_matches[t].size() >= 4)
 		{
-			std::cout<< file[t]+" found" << std::endl;
-//			m_status.data = t;
-			//m_statusOut.write();
 			if(gFlags & FLAG_DRAW)
 			{
-				for( int i = 0; i < t_good_matches[t].size(); i++ )
+				for(unsigned int i = 0; i < t_good_matches[t].size(); i++ )
 				{
 					t_ob[t].push_back( t_kp[t][ t_good_matches[t][i].queryIdx ].pt );
 					t_scene[t].push_back( kp_image[ t_good_matches[t][i].trainIdx ].pt );
@@ -230,28 +238,34 @@ for (int i=0; i<TARGET_COUNT; i++){
 				line( image, t_scene_corners[t][1], t_scene_corners[t][2], Scalar( 0, 255, 0), 4 );
 				line( image, t_scene_corners[t][2], t_scene_corners[t][3], Scalar( 0, 255, 0), 4 );
 				line( image, t_scene_corners[t][3], t_scene_corners[t][0], Scalar( 0, 255, 0), 4 );
+
+				large[t]=cv::norm(t_scene_corners[t][1]-t_scene_corners[t][0]);
+				width[t]=cv::norm(t_scene_corners[t][2]-t_scene_corners[t][1]);
+				area[t]=large[t]*width[t];
+				if(area[t]>15000 && area[t]<300000)
+				{
+					t_ob_center[t].push_back((t_scene_corners[t][0]+t_scene_corners[t][1])*.5);
+					std::cout<< "Area "+ file[t] + ": " << area[t] << + " and coord " << t_ob_center[t] << std::endl;
+				}
 			}
 		}
 	}
 
-        //Show detected matches
+	//Show detected matches
 	if(gFlags & FLAG_DRAW){
-	        imshow( "Result", image );
+		imshow( "Result", image );
 	}
 
-        key = waitKey(1);
 	delete[] t_matches;
 	delete[] t_good_matches;
 	delete[] t_ob;
+	delete[] t_ob_center;
 	delete[] t_scene;
 	delete[] t_scene_corners;
-    }
-
- delete[] t_images;
- delete[] t_descriptors;
- delete[] t_kp;
- delete[] t_ob_corners;
-
+	delete[] large;
+	delete[] width;
+	delete[] area;
+	coil::usleep(100000);
   return RTC::RTC_OK;
 }
 
