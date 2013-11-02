7d95ea692d5b48da02fd5ada688058418b931f22
Pedro Aguiar <paguiar32@gmail.com>
2013-11-02 08:03:46 -0600

    Dummyfied a little more

--- RTCs/hardware/src/Hardware.cpp
+++ RTCs/hardware/src/Hardware.cpp
@@ -1,11 +1,3 @@
-// -*- C++ -*-
-/*!
- * @file  Hardware.cpp
- * @brief Interface/filter between master and real/simulated robot
- * @date $Date$
- *
- * $Id$
- */
 #include <sstream>
 #include <iostream>
 #include <json/json.h>
@@ -23,8 +15,6 @@ enum {
 	V_TARGET
 };
 
-// Module specification
-// <rtc-template block="module_spec">
 static const char* hardware_spec[] =
   {
     "implementation_id", "Hardware",
@@ -32,7 +22,7 @@ static const char* hardware_spec[] =
     "description",       "Interface/filter between master and real/simulated robot",
     "version",           "0.0.1",
     "vendor",            "UDEM",
-    "category",          "Interfac",
+    "category",          "Interface",
     "activity_type",     "PERIODIC",
     "kind",              "DataFlowComponent",
     "max_instance",      "5",
@@ -44,7 +34,6 @@ static const char* hardware_spec[] =
     "conf.default.str_slopes", "0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5",
     ""
   };
-// </rtc-template>
 
 /* Evan Teran's string-splitting code from: http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c */
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
@@ -168,7 +157,6 @@ HardwareController::HardwareController(int j, std::vector<float> s, std::string 
 	setValues("-30,-10,-20,-34,-40,-20,10,0", V_CURRENT);
 }
 
-
 HardwareController::~HardwareController(void)
 {
 }
@@ -196,37 +184,25 @@ std::string HardwareController::getJSON(void){
  return toRobot.toStyledString();
 }
 
-
 class HardwareController* HW = NULL;
 
-
-/*!
- * @brief constructor
- * @param manager Maneger Object
- */
 Hardware::Hardware(RTC::Manager* manager)
-    // <rtc-template block="initializer">
+
   : RTC::DataFlowComponentBase(manager),
     m_p_positionIn("Position", m_p_position),
     m_p_feedbackIn("Feedback", m_p_feedback),
     m_p_statusOut("Status", m_p_status),
     m_p_dataOut("Data", m_p_data)
 
-    // </rtc-template>
 {
 }
 
-/*!
- * @brief destructor
- */
 Hardware::~Hardware()
 {
 }
 
 RTC::ReturnCode_t Hardware::onInitialize()
 {
-  // Registration: InPort/OutPort/Service
-  // <rtc-template block="registration">
   // Set InPort buffers
   addInPort("Position", m_p_positionIn);
   addInPort("Feedback", m_p_feedbackIn);
@@ -234,24 +210,13 @@ RTC::ReturnCode_t Hardware::onInitialize()
   // Set OutPort buffer
   addOutPort("Status", m_p_statusOut);
   addOutPort("Data", m_p_dataOut);
-  
-  // Set service provider to Ports
-  
-  // Set service consumers to Ports
-  
-  // Set CORBA Service Ports
-  
-  // </rtc-template>
 
-  // <rtc-template block="bind_config">
   bindParameter("int_joints", m_int_joints, "8");
   bindParameter("str_json_data_format", m_str_json_data_format, "<WRIST>([ANGLE.1:%][ANGLE.2:%][ANGLE.3:%][ANGLE.4:%][ANGLE.5:%][ANGLE.6:%][ANGLE.7:%][GRIPPER:%])");
   bindParameter("str_slopes", m_str_slopes, "0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5");
-  // </rtc-template>
 
  return RTC::RTC_OK;
 }
-
 
 RTC::ReturnCode_t Hardware::onFinalize()
 {
@@ -262,15 +227,6 @@ RTC::ReturnCode_t Hardware::onStartup(RTC::UniqueId ec_id)
 {
  return RTC::RTC_OK;
 }
-
-
-/*
-RTC::ReturnCode_t Hardware::onShutdown(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
 
 RTC::ReturnCode_t Hardware::onActivated(RTC::UniqueId ec_id)
 {
@@ -343,41 +299,6 @@ RTC::ReturnCode_t Hardware::onExecute(RTC::UniqueId ec_id)
 
  return RTC::RTC_OK;
 }
-
-/*
-RTC::ReturnCode_t Hardware::onAborting(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t Hardware::onError(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t Hardware::onReset(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t Hardware::onStateUpdate(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t Hardware::onRateChanged(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
 
 extern "C"
 {
